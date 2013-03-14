0db19ac64c05c185b170c3700bccb0dff152bfa1
Ajay Chainani <ajay.chainani@gmail.com>
2013-03-14 13:20:29 -0400

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,13 @@
+window.onresize = resize;
+window.onload = resize;
+
+function resize()
+{
+	
+	var iphone = document.getElementById("iphoneImage");
+	var body = document.body;
+	
+	iphone.style.top = Math.max(body.offsetHeight - iphone.offsetHeight, document.getElementById("textWrapper").offsetHeight) + "px";
+	iphone.style.left = (body.offsetWidth - iphone.offsetWidth)/2 + "px";
+			
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajayjapan::Application.config.secret_token = 'cc54d6c10413ec65cf63cb60095fcdc8148c77c42ef4f07aacb269a1d74f1d9175af678d9592cfe80f4a021b69375dfa5b23d8d62a2a8b768738b48fdf60c4c9'
