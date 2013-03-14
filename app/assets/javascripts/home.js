82478ca894abab9bd4b4585240267f2bec5203d9
Ajay Chainani <ajay.chainani@gmail.com>
2013-03-14 17:21:36 -0400

    bg image fix

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,5 +9,4 @@ function resize()
 	
 	iphone.style.top = Math.max(body.offsetHeight - iphone.offsetHeight, document.getElementById("textWrapper").offsetHeight) + "px";
 	iphone.style.left = (body.offsetWidth - iphone.offsetWidth)/2 + "px";
-			
 }
