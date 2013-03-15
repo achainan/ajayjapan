120818cc2364dc9e86c45c1164d50494e2c3c515
Ajay Chainani <ajay.chainani@gmail.com>
2013-03-14 21:20:46 -0400

    bg auto fit fix pt 3

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,8 +10,8 @@ function resize()
 	iphone.style.top = Math.max(body.offsetHeight - iphone.offsetHeight, document.getElementById("textWrapper").offsetHeight) + "px";
 	iphone.style.left = (body.offsetWidth - iphone.offsetWidth)/2 + "px";
 	
-	var imageSize = Math.max(body.offsetWidth, body.offsetHeight);
-	
+	var imageSize = Math.max(body.scrollWidth, body.scrollHeight);
+		
 	body.style.backgroundSize = imageSize + "px " + imageSize + "px";
 	
 }
